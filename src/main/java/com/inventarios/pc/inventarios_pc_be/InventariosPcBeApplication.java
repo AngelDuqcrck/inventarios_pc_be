f5431a88eec2e50896ffe1776f9a38ecd2af16a9
Angel Yesid Duque <angelyesidducr@ufps.edu.co>
2024-09-19 21:20:45 -0500

    first commit

---
+++ src/main/java/com/inventarios/pc/inventarios_pc_be/InventariosPcBeApplication.java
@@ -0,0 +1,14 @@
+package com.inventarios.pc.inventarios_pc_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventariosPcBeApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(InventariosPcBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inventarios/pc/inventarios_pc_be/InventariosPcBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventarios.pc.inventarios_pc_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventariosPcBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
